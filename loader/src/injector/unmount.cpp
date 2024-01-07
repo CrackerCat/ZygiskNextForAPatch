2a350af9f4256c593306b0baac5ee6c5efff97cb
Yervant7 <71306082+Yervant7@users.noreply.github.com>
2024-01-07 11:01:00 -0300

    fix unmount

--- loader/src/injector/unmount.cpp
+++ loader/src/injector/unmount.cpp
@@ -11,6 +11,7 @@ using namespace std::string_view_literals;
 namespace {
     constexpr auto MODULE_DIR = "/data/adb/modules";
     constexpr auto KSU_OVERLAY_SOURCE = "KSU";
+    constexpr auto APATCH_OVERLAY_SOURCE = "APatch";
     constexpr auto ZYGISK_FUSE_SOURCE = "zygisk";
     const std::vector<std::string> KSU_PARTITIONS{"/system", "/vendor", "/product", "/system_ext", "/odm", "/oem"};
 
@@ -67,13 +68,25 @@ void revert_unmount_ksu() {
 
 
 void revert_unmount_kpatch() {
+    std::string apatch_loop;
     std::vector<std::string> targets;
 
+    // Unmount apatch module dir last
     targets.emplace_back(MODULE_DIR);
 
     for (auto& info: parse_mount_info("self")) {
+        if (info.target == MODULE_DIR) {
+            apatch_loop = info.source;
+            continue;
+        }
         // Unmount everything mounted to /data/adb
         if (info.target.starts_with("/data/adb")) {
+            targets.emplace_back(info.target);
+        }
+        // Unmount APATCH overlays
+        if (info.type == "overlay"
+            && info.source == APATCH_OVERLAY_SOURCE
+            && std::find(KSU_PARTITIONS.begin(), KSU_PARTITIONS.end(), info.target) != KSU_PARTITIONS.end()) {
             targets.emplace_back(info.target);
         }
         // Unmount fuse
@@ -81,6 +94,13 @@ void revert_unmount_kpatch() {
             targets.emplace_back(info.target);
         }
     }
+    for (auto& info: parse_mount_info("self")) {
+        // Unmount everything from apatch loop except apatch module dir
+        if (info.source == apatch_loop && info.target != MODULE_DIR) {
+            targets.emplace_back(info.target);
+        }
+    }
+
     // Do unmount
     for (auto& s: reversed(targets)) {
         lazy_unmount(s.data());
